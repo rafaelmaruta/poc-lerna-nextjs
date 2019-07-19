0dbb750efb194ca6da0d5ef7389577b8eda70c40
Rafael Maruta <rafaelmaruta@gmail.com>
2019-07-19 17:50:50 -0300

    working with microbundler

---
+++ packages/my-components/dist/index.mjs
@@ -0,0 +1,2 @@
+import e from"react";export default function(){return e.createElement("div",null,"Meu querido componente")}
+//# sourceMappingURL=index.mjs.map
---
+++ packages/my-components/dist/index.umd.js
@@ -0,0 +1,2 @@
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t(require("react")):"function"==typeof define&&define.amd?define(["react"],t):e.myComponents=t(e.react)}(this,function(e){return e=e&&e.hasOwnProperty("default")?e.default:e,function(){return e.createElement("div",null,"Meu querido componente")}});
+//# sourceMappingURL=index.umd.js.map
